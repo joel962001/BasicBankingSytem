6396d7aa51aad83e4f466476f387663ed481db35
joel962001 <joel962001@gmail.com>
2021-11-12 02:25:41 +0530

    setting change

--- bank/settings.py
+++ bank/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3nrdr6587acmh',
-        'USER': 'udejroksagtrin',
-        'PASSWORD': '599b9072e3e37d918717e58149524de754dbc484288c1a5be0bef1458b41102b',
-        'HOST': 'ec2-52-4-100-65.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
