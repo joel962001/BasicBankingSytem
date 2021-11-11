0b15ccf7f6bc6392437ddb0e24119a5015504bae
joel962001 <joel962001@gmail.com>
2021-11-12 02:14:28 +0530

    setting modify

--- bank/settings.py
+++ bank/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,16 +78,20 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3nrdr6587acmh',
-        'USER': 'udejroksagtrin',
-        'PASSWORD': '599b9072e3e37d918717e58149524de754dbc484288c1a5be0bef1458b41102b',
-        'HOST': 'ec2-52-4-100-65.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd3nrdr6587acmh',
+#         'USER': 'udejroksagtrin',
+#         'PASSWORD': '599b9072e3e37d918717e58149524de754dbc484288c1a5be0bef1458b41102b',
+#         'HOST': 'ec2-52-4-100-65.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
